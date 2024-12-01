46c284b22eeae43a525818de492b2c3f73a58c73
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2024-12-01 14:45:36 -0800

    move Window class to separate file

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window
 
-class Window:
-  def __init__(self, width, height):
-    self.width = width
-    self.height = height
-    self.__root = Tk()
-    self.title = self.__root
-    self.canvas = Canvas()
-    self.canvas.pack()
-    self.running = False
-    self.__root.protocol("WM_DELETE_WINDOW", self.close)
+def main():
+  win = Window(800, 600)
+  win.wait_for_close()
 
-  def redraw(self):
-    self.__root.update_idletasks()
-    self.__root.update()
-
-  def wait_for_close(self):
-    self.running = True
-    while self.running:
-      self.redraw()
-
-  def close(self):
-    self.running = False
+main()
